d78df6ed5d1cdee8a104d544796ba478a62d91ae
cocoa <jwv0323@naver.com>
2023-01-13 15:18:05 +0900

    Koala 01. 완전탐색 모든 순열 수정

--- Koala/01. 완전탐색/모든 순열.js
+++ Koala/01. 완전탐색/모든 순열.js
@@ -1,14 +1,17 @@
 const N = require('fs').readFileSync("/dev/stdin").toString().trim();
 let numbers = Array.from({length: N}, (_, i) => i+1).join('');
-let dictionaryOrder = [];
+let answer = [];
 
-function permutation(numbers, combination) {
-    if(numbers === '') dictionaryOrder.push(combination.split('').join(' '));
-    for (let i = 0; i < numbers.length; i++) {
-        // (fixed를 제외한 나머지, fixed) 
-        permutation(numbers.slice(0, i) + numbers.slice(i+1), combination + numbers[i]);       
+function permutation(num, s) {
+    // 1. 재귀함수를 멈춰야할 조건
+    if(num === '') answer.push(s.split('').join(' '));
+
+    // 2. 재귀를 돌면서 변경되어야 될 부분
+    for (let i = 0; i < num.length; i++) {
+        // (num[i]를 제외한 나머지, 현재 쌓은 스택 + num[i]) 
+        permutation(num.slice(0, i) + num.slice(i + 1), s + num[i]);       
     }
 }
 
-permutation(numbers, '')
-console.log(dictionaryOrder.join("\n"));
+permutation(numbers, '');
+console.log(answer.join('\n'));
